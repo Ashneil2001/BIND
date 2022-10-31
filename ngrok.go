598083556ee062e9be9de53932ad05bbfdce6da9
Ashneil2001 <87877825+Ashneil2001@users.noreply.github.com>
2022-10-31 12:46:04 +0530

    Add files via upload

---
+++ ngrok.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetMethod(c *gin.Context) {
+	fmt.Println("\n'GetMethod' called")
+	IdValue := c.Params.ByName("IdValue")
+	message := "GetMethod Called With Param: " + IdValue
+	c.JSON(http.StatusOK, message)
+	ReqPayload := make([]byte, 1024)
+	ReqPayload, err := c.GetRawData()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Request Payload Data: ", string(ReqPayload))
+}
+func main() {
+	router := gin.Default()
+	subRouterAuthenticated := router.Group("/api/v1/PersonId", gin.BasicAuth(gin.Accounts{
+		"arv": "824200",
+	}))
+	subRouterAuthenticated.GET("/:IdValue", GetMethod)
+	listenPort := "12640"
+
+	router.Run(":" + listenPort)
+}
